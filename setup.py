b64aa010181c5bbfe15a3f200a69f225c31fb8b7
Sam Dozor <sdozor@mparticle.com>
2016-06-14 17:12:49 -0400

    Update setup, new description, classifiers

--- setup.py
+++ setup.py
@@ -20,21 +20,27 @@ REQUIRES = ["urllib3 >= 1.15", "six >= 1.10", "certifi", "python-dateutil"]
 setup(
     name=NAME,
     version=VERSION,
-    description="""`Package Documentation <https://github.com/mParticle/mparticle-python-sdk/wiki>`_.
-    This SDK is a helper library for the mParticle server-to-server HTTP API, it exposes mParticle's schema as simple models and provides an HTTP client interface. This SDK is stateless and will only send the data that you populate, whereas our mobile SDKs will automatically collect app and device information, session events, install events, and maintain persistence.""",
+    description='Python client for the mParticle platform',
+    long_description='`Package Documentation <https://github.com/mParticle/mparticle-python-sdk/wiki>`_ \n\nThis SDK is a helper library for the mParticle server-to-server HTTP API, it exposes mParticle\'s schema as simple models and provides an HTTP client interface. This SDK is stateless and will only send the data that you populate, whereas our mobile SDKs will automatically collect app and device information, session events, install events, and maintain persistence.',
+    author='Sam Dozor',
     author_email="support@mparticle.com",
-    url="http://www.mparticle.com",
-    keywords=["mParticle"],
+    url="https://github.com/mParticle/mparticle-python-sdk",
+    keywords=["mparticle analytics marketing"],
     install_requires=REQUIRES,
     packages=find_packages(),
     include_package_data=True,
-    classifiers=[
-    'Development Status :: 4 - Beta', 
-    'Environment :: Console', 
-    'License :: OSI Approved :: Apache Software License', 
-    'Programming Language :: Python', 
-    'Topic :: Software Development :: Libraries :: Python Modules',
-    ],
+    license='Apache 2.0',
+    classifiers=(
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ),
 )
 
 
